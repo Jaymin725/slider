a02d8a3077bf9c8a47bfd712193d5328f162f9e2
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-01-21 21:17:26 +0530

    project almost complete...

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,47 @@
-const slider_items = document.querySelectorAll(".slider-item")
-const prev_button = document.querySelector("#slider-prev")
-const next_button = document.querySelector("#slider-next")
+const slider_items = document.querySelectorAll(".slider-item");
+const prev_button = document.querySelector("#slider-prev");
+const next_button = document.querySelector("#slider-next");
 
 var slider = {
   len: slider_items.length,
   index: 0,
 
-  previous: function () {
-    let prev = this.index - 1
-    return (prev < 0) ? (this.len - 1) : (prev)
+  previous: function (n = 1) {
+    let prev = this.index - n;
+    return (prev < 0) ? (this.len - n) : (prev);
   },
 
-  next: function () {
-    let next = this.index + 1
-    return (next > this.len - 1) ? (0) : (next)
+  next: function (n = 1) {
+    let next = this.index + n;
+    return (next > this.len - 1) ? (n - 1) : (next);
   },
 
   display: function () {
-    console.log(`prev: ${slider.previous()}, i: ${slider.index}, next: ${slider.next()}`)
+    console.log(`prev-prev: ${slider.previous(slider.previous())}, prev: ${slider.previous()}, i: ${slider.index}, next: ${slider.next()}, next-next: ${slider.next(slider.next())}`);
   },
 
   slidePrevious: function () {
-    slider.index = slider.previous()
-    slider.display()
+    slider.index = slider.previous();
+    slider.display();
+    setIndex();
   },
 
   slideNext: function () {
-    slider.index = slider.next()
-    slider.display()
+    slider.index = slider.next();
+    slider.display();
+    setIndex();
   }
 }
 
-function colorCode() {
-  slider_items.forEach((slide) => slide.style.color = "black")
+prev_button.addEventListener("click", slider.slidePrevious);
+next_button.addEventListener("click", slider.slideNext);
 
-  slider_items[slider.index].style.color = "#00ff00"
-  slider_items[slider.previous()].style.color = "#ff0000"
-  slider_items[slider.next()].style.color = "#0000ff"
+function setIndex() {
+  slider_items.forEach((slide, i) => {
+    // slide.classList.toggle("previous-previous", i == slider.previous(2));
+    slide.classList.toggle("previous", i == slider.previous());
+    slide.classList.toggle("active", i == slider.index);
+    slide.classList.toggle("next", i == slider.next());
+    // slide.classList.toggle("next-next", i == slider.next(2));
+  })
 }
-
-prev_button.addEventListener("click", () => {
-  slider.slidePrevious()
-  colorCode()
-})
-
-next_button.addEventListener("click", () => {
-  slider.slideNext()
-  colorCode()
-})
-
-colorCode()
